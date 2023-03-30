621d36eaae86add8be1b23f726a0e54aac52e4d4
doha fekouss <63255138+dohafekouss@users.noreply.github.com>
2023-03-30 09:52:25 +0100

    Delete 27.15.py

--- 27.15.py
+++
@@ -1,8 +0,0 @@
-initialVelocity=int(input())
-rocketHeight=0
-timeSinceLaunch=0
-while rocketHeight>=0:
-    print(timeSinceLaunch," ",rocketHeight)
-    timeSinceLaunch=timeSinceLaunch+1
-
-    rocketHeight = (initialVelocity * timeSinceLaunch) - (5 * timeSinceLaunch * timeSinceLaunch)
